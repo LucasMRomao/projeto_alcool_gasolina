ce59af82710f001cfa626146f031d6be737719a3
LucasMRomao <lmartins.romao@gmail.com>
2022-08-11 21:18:28 -0300

    Implementado Vue.JS no projeto

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 window.onload = function(){
-    document.getElementById("bVerificar").onclick = function(){
-        let valorAlcool = document.getElementById("iPrecoAlcool").value;
-        let valorGasolina = document.getElementById("iPrecoGasolina").value;
-
-        if(!valorAlcool || !valorGasolina){
-            alert("Por favor, insira os 2 valores!");
-        }else{
-            let resultado = Number(valorAlcool) / Number(valorGasolina);
-
-            resultado <= 0.7 ? document.getElementById("sResultado").textContent = "É mais vantajoso usar álcool!" : document.getElementById("sResultado").textContent = "É mais vantajoso usar gasolina!";
+    var app = new Vue({
+        el: "#app",
+        data: {
+            alcool: '',
+            gasolina: '',
+            resultado: ''
+        },
+        watch: {
+            alcool: function(){
+                this.resultado = 'Aguardando digitação...';
+                this.debouncedCalculo();
+            },
+            gasolina: function(){
+                this.resultado = 'Aguardando digitação...';
+                this.debouncedCalculo();
+            }
+        },
+        created: function(){
+            this.debouncedCalculo = _.debounce(this.calcular, 500);
+        },
+        methods: { 
+            calcular: function(){
+                if(!this.alcool || !this.gasolina){
+                    this.resultado = 'Ambos os valores devem ser informados...';
+                    return;
+                }
+                
+                let result = Number(this.alcool) / Number(this.gasolina);
+                this.resultado = result <= 0.7 ? 'É mais vantajoso usar álcool!' : 'É mais vantajoso usar gasolina!';
+            }
         }
-    }
+    });
 }
